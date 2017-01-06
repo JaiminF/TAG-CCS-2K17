ec048bc59f2eb1ef1b8cd1dca9fe9dd8719b3fdb
Pratik Khandelwal <prkhandelwal@hotmail.com>
2017-01-07 00:26:47 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,42 @@
+<?php
+$user   = "Hack";
+$password ="MGVh5MAs9fHaxQcK";
+$database = "ccs";
+$host   = "localhost";
+$con = mysqli_connect($host,$user,$password,$database);
+if (!$con)
+
+  {
+
+  die('Could not connect: ' . mysqli_error());
+
+  }
+
+mysqli_select_db($con,$database);
+
+$token = rand(); 
+
+
+$sql="INSERT INTO `r_users` (`Name`,`R_no`,`contact`,`tech`,`mang`,`design`,`editorial`,`token`)
+
+VALUES
+
+('$_POST[fullname]','$_POST[regno]','$_POST[phonenumber]','$_POST[tech]','$_POST[mang]','$_POST[design]','$_POST[edit]','$token')";
+
+ 
+
+if (!mysqli_query($con,$sql))
+
+  {
+
+  die('Error: ' . mysqli_error($con));
+
+  }
+
+
+ 
+
+mysqli_close($con)
+
+?>
+<script type="text/javascript">location.href = 'success.html';</script>
